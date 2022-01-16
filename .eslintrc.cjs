02610880f8085a48446c98059f614cb1a6033cad
borsTiHD <bastian.j@gmx.net>
2022-01-16 16:27:11 +0100

    🚨 Linter >> Adding rules.

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {},
+    extends: [],
+    plugins: [],
+    // add your custom rules here
+    rules: {
+        indent: ['error', 4, {
+            ignoredNodes: ['TemplateLiteral'],
+            SwitchCase: 1
+        }
+        ],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'comma-dangle': ['error', 'never'],
+        'linebreak-style': ['error', 'windows'],
+        'template-curly-spacing': 'off',
+        'max-len': ['off', { code: 120 }],
+        'no-console': 'off',
+        'arrow-parens': ['error', 'always'],
+        'space-before-function-paren': ['error', 'never'],
+        curly: ['error', 'multi-line'],
+        'import/no-extraneous-dependencies': 'off',
+        'require-await': 0,
+
+        'global-require': 0,
+        'import/no-unresolved': 0,
+        'import/newline-after-import': 0,
+        'no-underscore-dangle': 0,
+    }
+}
