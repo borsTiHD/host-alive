8c41a734f3c23d36b8216f0e4c18d0f0fdbe0c69
borsTiHD <bastian.j@gmx.net>
2022-01-17 09:45:48 +0100

    ✨ App >> IP-Adresses are accepted now.

--- src/resolve-host-ip.js
+++ src/resolve-host-ip.js
@@ -1,7 +1,11 @@
 import http from 'http'
+import testIpAdress from './regex-ipv4-ipv6.js'
 
 // Resolves IP Adress for given URL
 export default (host) => {
+    // Test if given host is a IP-Adress or a host
+    // IPv4/IPv6 Adresses returning directly
+    if (testIpAdress(host)) { return host }
     return new Promise((resolve, reject) => {
         http.get(host, function(res) {
             const remoteAdress = res.socket.remoteAddress
